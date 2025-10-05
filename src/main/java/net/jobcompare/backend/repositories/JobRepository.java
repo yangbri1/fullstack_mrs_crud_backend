ecc551bf0a58c2344c4f0c1b1794a989a543ebf0
brian <yangbri1@msu.edu>
2025-10-05 02:22:09 -0400

    started on the 'JobRepository.java' class respective to 'Job.java' '@Entity' class, the 'JobRepository' extends JpaRepository s.t. it could borrow its built-in default CRUD operations for the creation, retrieval, update, and deletion of job openings --- side note H2 console displays the schema fields/columns in alphabetical order when 'SELECT * FROM jobs' BUT internally Spring Boot still maps results in correct order using JPA

---
+++ src/main/java/net/jobcompare/backend/repositories/JobRepository.java
@@ -0,0 +1,25 @@
+package net.jobcompare.backend.repositories;
+
+// import 'List' data structure from Java's utility class
+import java.util.List;
+
+// Spring DATA JPA API repository provides many default CRUD operations 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+// import 'Job.java' '@Entity' class
+import net.jobcompare.backend.entities.*;
+
+// 'JobRepository.java' class inherits the CRUD operations and attributes from 'JpaRepository' (which takes in 'Job' entity class & 'jobId' data type as args)
+public interface JobRepository extends JpaRepository<Job, Integer> {
+
+    /* OMITTED: employed built-in .findlAll() method from 'JPARepository' for these CRUD functionalities */
+//     public List<Job> getAllJobs();
+//     public Job findByJobId(Integer jobId);
+//     public Job updateByJobId(Integer jobId, Job job);
+//     public Job deleteByJobId(Integer jobId);
+//     // '@Query' annotation from JPA to retrieve all job openings posted by one particular recruiter
+//     // Aside: 'recruiterId' is the PRIMARY_KEY in its own table while also being a FOREIGN_KEY to 'jobs' DB table, connecting the two
+//     @Query("SELECT opening FROM jobs WHERE opening.teamId = :recruiterId")
+//     public List<Job> findAllJobsByTeamId(Integer recruiterId);
+}
