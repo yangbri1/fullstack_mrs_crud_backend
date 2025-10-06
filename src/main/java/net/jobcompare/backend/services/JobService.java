4e1dba3a990c83c31eeedb0f3637adfc9643572f
brian <yangbri1@msu.edu>
2025-10-05 21:50:52 -0400

    'JobService.java' class believe to enable 'Job' with full CRUD functionalities ... may need improvements

--- src/main/java/net/jobcompare/backend/services/JobService.java
+++ src/main/java/net/jobcompare/backend/services/JobService.java
@@ -28,7 +28,7 @@ public class JobService {
     /* introduce 'JobRepository' obj in 'JobRepository' obj -- enables 'UserService' class to use 'JobRepository' functionalities */
     public JobService(JobRepository jobRepository){
         /* reserved keyword 'this' points to earlier declared instance variable (outside this constructor) ...
-        & assign given parameter 'messageRepository' value to it */
+        & assign given parameter 'jobRepository' value to it */
         this.jobRepository = jobRepository;
     }
 
@@ -100,7 +100,7 @@ public class JobService {
         if(jobOptional.isPresent()){
             // grab the value via .get() method from 'java.util.Optional' package
             Job position = jobOptional.get();
-            // persist changes to 'Message' DB table
+            // persist changes to 'Job' DB table
             jobRepository.save(position);
             return position;
         }
@@ -116,9 +116,57 @@ public class JobService {
     //     }
     // }
 
-    public Integer updateByJobId(Integer jobId, Job job) throws Exception{
+    public Job updateByJobId(Integer jobId, Job job) throws Exception{
+        // initialization block for all the schema fields
+        String jobTitle = job.getTitle();
+        String jobCompany = job.getCompany();
+        String jobLocation = job.getLocation();
+        String jobDescription = job.getDescription();
+        String jobEmail = job.getHiringTeamEmail();
+
+        Integer yearOfExperience = job.getYearOfExperience();
+        Integer costOfLiving = job.getCostOfLivingIndex();
+        // Integer moderatorId = job.getModId();
+
+        // Long timeOfPosting = job.getTimeOfPosting();
+        Float yearlySalary = job.getYearlySalary();
+
+        // if job description fields does NOT contain anything ...
+        if(jobTitle.isEmpty() || jobCompany.isEmpty() || jobLocation.isEmpty() || jobDescription.isEmpty() || jobEmail.isEmpty()){
+            throw new Exception("Schema field is empty [UPDATE]");
+        }
         
+        // else if any of the core job fields are above the character limit ...
+        if(jobTitle.length() > 60 || jobCompany.length() > 20|| jobLocation.length() > 85 || jobDescription.length() > 4000 || jobEmail.length() > 60){
+            throw new Exception("Schema field exceeds length [UPDATE]");
+        }
+
+        // else if 'jobId' DNE ...
+        /* EQUIVALENT to 'if(jobOptional.isEmpty()){...}' logic-wise, req. Optional to be declare earlier*/
+        if(!jobRepository.existsById(job.getJobId())){  // job.getModId()
+            throw new Exception("jobId field DNE [UPDATE]");
+        }
+
+        // create an 'Optional' obj
+        // call .findById() method to retrieve 'Job' record from DB fitting given parameter 'jobId'
+        Optional<Job> jobOptional = jobRepository.findById(job.getJobId());
+        // if value assigned to 'jobOptional' container obj's value is NON-null
+        if(jobOptional.isPresent()){
+            // grab the value via .get() method from 'java.util.Optional' package
+            Job position = jobOptional.get();
+            // extract 'description' field from provided 'Job' arg & use setter function to overwrite old 'description'
+            position.setDescription(job.getDescription());
+            // persist changes to 'Job' DB table
+            jobRepository.save(position);
+            // tying up loose ends 
+            jobRepository.deleteById(jobId);
+            // return updated job offering
+            return position;    // return 1
+        }
+        // otw if value from 'jobOptional' is indeed NULL ... return default falsy value (null)
+        return null;
     }
+
     public Integer deleteJob(Integer jobId){    // void
         // call .findById() method to retrieve 'Job' entity/record from DB fitting given parameter 'jobId'
         Optional<Job> jobOptional = jobRepository.findById(jobId);
